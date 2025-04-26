a886090922eb60f72d33b78d3e046d170ee49a21
Alok Patel <alokpatel23273@gmail.com>
2025-04-27 00:45:25 +0530

    commited

--- components/Account.js
+++ components/Account.js
@@ -3,94 +3,92 @@ export default function Account() {
   return (
     <>
       <div className="flex-1  pl-1 text-center account">
-        <div className="account1">
-          <div className="process">
-            <h2 className="!ml-12">Account Progress</h2>
-            <div className="outerCircle">
-              <div className="innerCircle">
-                <b className=" Data">85% </b>
-              </div>
+        <div className="process">
+          <h2 className="!ml-12">Account Progress</h2>
+          <div className="outerCircle">
+            <div className="innerCircle">
+              <b className=" Data">85% </b>
             </div>
           </div>
-          <div className="steps ">
-            <div className="completed">
-              <h1 className="pt-2 mr-28">
-                <b>Steps Completed</b>
-              </h1>
-              <ul
-                className=" w-vh ml-10 mt-2"
-                style={{ listStyleType: "circle" }}
-              >
-                <li>
-                  <label className="mr-25" htmlFor="profile">
-                    Profile Setup
-                  </label>
-                  <input
-                    className="accent-green-500"
-                    type="checkbox"
-                    id="profile"
-                    name="profile"
-                  />
-                </li>
+        </div>
+        <div className="steps ">
+          <div className="completed">
+            <h1 className="pt-2 mr-28">
+              <b>Steps Completed</b>
+            </h1>
+            <ul
+              className=" w-vh ml-10 mt-2"
+              style={{ listStyleType: "circle" }}
+            >
+              <li>
+                <label className="mr-25" htmlFor="profile">
+                  Profile Setup
+                </label>
+                <input
+                  className="accent-green-500"
+                  type="checkbox"
+                  id="profile"
+                  name="profile"
+                />
+              </li>
 
-                <li>
-                  <label className="mr-23" htmlFor="training">
-                    Initial Training
-                  </label>
-                  <input
-                    type="checkbox"
-                    id="training"
-                    name="training"
-                    className="accent-green-500"
-                  />
-                </li>
+              <li>
+                <label className="mr-23" htmlFor="training">
+                  Initial Training
+                </label>
+                <input
+                  type="checkbox"
+                  id="training"
+                  name="training"
+                  className="accent-green-500"
+                />
+              </li>
 
-                <li>
-                  <label className="mr-17" htmlFor="documents">
-                    Legal Documents
-                  </label>
-                  <input
-                    type="checkbox"
-                    id="documents"
-                    name="documents"
-                    className="accent-green-500"
-                  />
-                </li>
-              </ul>
-            </div>
-            <div className="remaining ">
-              <h1 className="pt-2 mr-26">
-                <b>Steps Remaining</b>
-              </h1>
-              <ul
-                className=" w-vh ml-10 mt-2"
-                style={{ listStyleType: "circle" }}
-              >
-                <li>
-                  <label className="mr-13" htmlFor="Financial">
-                    Financial Integration
-                  </label>
-                  <input
-                    type="checkbox"
-                    id="Financial"
-                    name="Financial"
-                    className="accent-green-500"
-                  />
-                </li>
+              <li>
+                <label className="mr-17" htmlFor="documents">
+                  Legal Documents
+                </label>
+                <input
+                  type="checkbox"
+                  id="documents"
+                  name="documents"
+                  className="accent-green-500"
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="remaining ">
+            <h1 className="pt-2 mr-26">
+              <b>Steps Remaining</b>
+            </h1>
+            <ul
+              className=" w-vh ml-10 mt-2"
+              style={{ listStyleType: "circle" }}
+            >
+              <li>
+                <label className="mr-13" htmlFor="Financial">
+                  Financial Integration
+                </label>
+                <input
+                  type="checkbox"
+                  id="Financial"
+                  name="Financial"
+                  className="accent-green-500"
+                />
+              </li>
 
-                <li>
-                  <label className="mr-26" htmlFor="Review">
-                    Final Review
-                  </label>
-                  <input
-                    type="checkbox"
-                    id="Review"
-                    name="Review"
-                    className="accent-green-500"
-                  />
-                </li>
-              </ul>
-            </div>
+              <li>
+                <label className="mr-26" htmlFor="Review">
+                  Final Review
+                </label>
+                <input
+                  type="checkbox"
+                  id="Review"
+                  name="Review"
+                  className="accent-green-500"
+                />
+              </li>
+            </ul>
           </div>
         </div>
       </div>
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,26 @@
+"use client";
 import "../app/page.css";
+import { useState } from "react";
 export default function Sidebar() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
   return (
     <>
-      <div className="row-span-3 p-4 SideNavigation grid grid-rows-[1fr_100px]">
+      <button
+        onClick={toggleSidebar}
+        className="md:hidden  bg-sky-800 w-8 h-5 mb-5 ml-3 text-white fixed top-4 left-4 z-50 text-xs"
+      >
+        ☰
+      </button>
+
+      <div
+        className={`row-span-3 p-4 SideNavigation grid grid-rows-[1fr_100px] ${
+          showSidebar ? "translate-x-0" : "-translate-x-full"
+        } md:relative md:translate-x-0 md:flex md:flex-col md:w-64 md:h-auto`}
+      >
         <div className="row-start-1 Links">
           <a className="HomeLink" href="">
             Home
@@ -19,7 +37,9 @@ export default function Sidebar() {
             <p className="PendingQs">3</p>
           </a>
         </div>
-        <button className="row-start-2 mr-36 text-gray-400">Logout</button>
+        <button className="row-start-2 mr-36 text-gray-400 mt-auto mb-2 mr-1">
+          Logout
+        </button>
       </div>
     </>
   );
